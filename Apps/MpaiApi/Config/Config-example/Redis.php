8ab121389da19f9a07119333d5c156e649a79148
taozywu <taozywu@users.noreply.github.com>
2022-05-07 10:31:37 +0800

    Update Redis.php

--- Apps/MpaiApi/Config/Config-example/Redis.php
+++ Apps/MpaiApi/Config/Config-example/Redis.php
@@ -17,7 +17,7 @@ class Redis
      * @var array
      */
     public $default = array('nodes' => array(
-        array('master' => "10.25.76.133:7008", 'slave' => "10.25.76.133:7008"),
+        array('master' => "localhost:7008", 'slave' => "localhost:7008"),
         ),
         'db' => 1
     );
